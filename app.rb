2510ea60ebfb4a4df2f3edb609cfa392f560f9c8
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-01-16 16:19:39 +0000

    Using parameters

--- app.rb
+++ app.rb
@@ -16,7 +16,13 @@ get '/info' do
   "Working through Makers Academy challenge"
 end
 
-get '/cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample 
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
