4dc6e0ca76f988d83a07cf67f7729984b3cc28da
Kat Hicks <kmhicks92@gmail.com>
2017-01-16 15:17:35 +0000

    Installed shotgun gem for automatic reloading and added some extra routes

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello!"
 end
+
+get '/secret' do
+  "Hello, world!"
+end
+
+get '/additional' do
+  "Testing shotgun gem!"
+end
+
+get '/info' do
+  "Working through Makers Academy challenge"
+end
